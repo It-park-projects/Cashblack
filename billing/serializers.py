7850d71bc1e6eca29ac99ff51bce78df01a61608
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-01-05 02:10:16 -0800

    'dsdsdsd'

--- billing/serializers.py
+++ billing/serializers.py
@@ -57,14 +57,8 @@ class CreateNotificationSmsSerializers(serializers.ModelSerializer):
         notification_create = NotifikationsSendClient.objects.create(**validate_date)
         notification_create.shop_id = self.context.get('shop_id')
         notification_create.author_id = self.context.get('author_id')
-        #     title = validate_date['title'],
-        #     content = validate_date['content'], 
-        #     author_id = self.context.get('author_id'),
-        #     img = validate_date['img'].FILES,
-        #     shop_id = self.context.get('shop_id')
-        # )   
-        
         return notification_create
+
     def update(self,instance,validate_data):
         instance.title = validate_data.get('title',instance.title)
         instance.content = validate_data.get('content',instance.content)
